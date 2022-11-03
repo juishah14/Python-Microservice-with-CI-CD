712518a846e18c5bd763879ce9328b20525cd0c6
juishah14 <juishah14@gmail.com>
2022-11-03 11:24:21 -0400

    fix imports

--- library/logic.py
+++ library/logic.py
@@ -1,26 +1,24 @@
-# import wikipedia
-# from textblob import TextBlob
+import wikipedia
+from textblob import TextBlob
 
 
 def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
 
-    # my_wiki = wikipedia.summary(name, length)
-    # return my_wiki
-    result = "Wow a beautiful war goddess"
-    return result
+    my_wiki = wikipedia.summary(name, length)
+    return my_wiki
 
 
 def search_wiki(name):
     """Search Wikipedia for Names"""
 
-    # results = wikipedia.search(name)
-    # return results
+    results = wikipedia.search(name)
+    return results
 
 
 def phrase(name):
     """Returns phrases from wikipedia"""
 
     page = wiki(name)
-    # blob = TextBlob(page)
-    # return blob.noun_phrases
+    blob = TextBlob(page)
+    return blob.noun_phrases
